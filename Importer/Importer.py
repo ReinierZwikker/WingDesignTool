952570d532f5c097b8b74c1b3459bce46a9e73fb
Reinier Zwikker <reinier@zwikker.eu>
2020-11-12 15:41:52 +0100

    Fixes in Importer

--- Importer/Importer.py
+++ Importer/Importer.py
@@ -1,31 +1,51 @@
 import numpy as np
 import scipy as sp
 from scipy import interpolate
+from Database.database_functions import DatabaseConnector
+import os
+import inspect
+database_connector = DatabaseConnector()
+
+aerodynamic_data_file_0 = '\MainWing_a0.00_v10.00ms.txt'
+aerodynamic_data_file_10 = '\MainWing_a10.00_v10.00ms.txt'
 
 
 """
 Output of this function is a numpy array, where [1] is the second data point etc. In that data point:
 [0] is the y-span, [1] is the chord, [2] the induced angle of attack, [3] the Cl, [5] the induced Cd, [7] is Cm around c/4
 """
+
+
 def main_wing_xflr5(name):
-    main_wing = np.genfromtxt(name, skip_header=40, skip_footer=1029, usecols=(0,1,2,3,5,7))
+    main_wing = np.genfromtxt(name, skip_header=40, skip_footer=1029, usecols=(0, 1, 2, 3, 5, 7))
     return main_wing
 
-def rolling_moment_coef_xlfr5(name):
-    f = open(name, 'r')
-    lines = f.readlines()
-    rolling_moment_coef = float(lines[12].split()[2])
-    f.close()
-    return rolling_moment_coef
 
-data = main_wing_xflr5('MainWing_a0.00_v10.00ms.txt')
-rolling_moment_coef = rolling_moment_coef_xlfr5('MainWing_a0.00_v10.00ms.txt')
+def rolling_moment_coef_xlfr5(name):
+    with open(name, 'r') as file:
+        lines = file.readlines()
+        return float(lines[12].split()[2])
+
+
+importer_folder = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+aerodynamic_data_0 = main_wing_xflr5(importer_folder + aerodynamic_data_file_0)
+aerodynamic_data_10 = main_wing_xflr5(importer_folder + aerodynamic_data_file_10)
+rolling_moment_coef_0 = rolling_moment_coef_xlfr5(importer_folder + aerodynamic_data_file_0)
+rolling_moment_coef_10 = rolling_moment_coef_xlfr5(importer_folder + aerodynamic_data_file_10)
 
 """
 Y-span varies from 0 to 
 """
-chord = sp.interpolate.interp1d(data[:,0], data[:,1], kind='cubic', fill_value="extrapolate")
-angle_induced = sp.interpolate.interp1d(data[:,0], data[:,2], kind='cubic', fill_value="extrapolate")
-lift_coef = sp.interpolate.interp1d(data[:,0], data[:,3], kind='cubic', fill_value="extrapolate")
-drag_induced = sp.interpolate.interp1d(data[:,0], data[:,4], kind='cubic', fill_value="extrapolate")
-moment_coef = sp.interpolate.interp1d(data[:,0], data[:,5], kind='cubic', fill_value="extrapolate")
+chord_function = sp.interpolate.interp1d(aerodynamic_data_0[:, 0], aerodynamic_data_0[:, 1], kind='cubic', fill_value="extrapolate")
+
+# 0 aoa
+angle_induced_function_0 = sp.interpolate.interp1d(aerodynamic_data_0[:, 0], aerodynamic_data_0[:, 2], kind='cubic', fill_value="extrapolate")
+lift_coef_function_0 = sp.interpolate.interp1d(aerodynamic_data_0[:, 0], aerodynamic_data_0[:, 3], kind='cubic', fill_value="extrapolate")
+drag_induced_function_0 = sp.interpolate.interp1d(aerodynamic_data_0[:, 0], aerodynamic_data_0[:, 4], kind='cubic', fill_value="extrapolate")
+moment_coef_function_0 = sp.interpolate.interp1d(aerodynamic_data_0[:, 0], aerodynamic_data_0[:, 5], kind='cubic', fill_value="extrapolate")
+
+# 10 aoa
+angle_induced_function_10 = sp.interpolate.interp1d(aerodynamic_data_10[:, 0], aerodynamic_data_10[:, 2], kind='cubic', fill_value="extrapolate")
+lift_coef_function_10 = sp.interpolate.interp1d(aerodynamic_data_10[:, 0], aerodynamic_data_10[:, 3], kind='cubic', fill_value="extrapolate")
+drag_induced_function_10 = sp.interpolate.interp1d(aerodynamic_data_10[:, 0], aerodynamic_data_10[:, 4], kind='cubic', fill_value="extrapolate")
+moment_coef_function_10 = sp.interpolate.interp1d(aerodynamic_data_10[:, 0], aerodynamic_data_10[:, 5], kind='cubic', fill_value="extrapolate")
