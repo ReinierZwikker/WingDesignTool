77c9343fb9a43e514c88031775e212abf94da7e3
Reinier Zwikker <reinier@zwikker.eu>
2020-11-17 14:21:38 +0100

    Added catch for system path error

--- InertialLoadingCalculator/InertialLoadingCalculator.py
+++ InertialLoadingCalculator/InertialLoadingCalculator.py
@@ -1,44 +1,51 @@
 ### ------ Workpackage 4.1: Inertial loading calculations ------ ###
 import scipy
 import numpy as np
-from Database.database_functions import DatabaseConnector
+
+try:
+    import Importer.xflr5
+    from Database.database_functions import DatabaseConnector
+except ModuleNotFoundError:
+    import sys
+    from os import path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+    import Importer.xflr5
+    from Database.database_functions import DatabaseConnector
 
 database_connector = DatabaseConnector()
-import Importer.xflr5
 
-#Import basic geometry
+# Import basic geometry
 wing_span = database_connector.load_value("wing_span")
 Outer_diameter = database_connector.load_value("df,outer")
-radius_fuselage = Outer_diameter/2
+radius_fuselage = Outer_diameter / 2
 surface_area = database_connector.load_value("surface_area")
 
-#Define the flight conditions
-velocity = 0.0 #m/s
-density = 0.0 #kg/m^2
+# Define the flight conditions
+velocity = 0.0  # m/s
+density = 0.0  # kg/m^2
 
-#Import weight and location of the engine
+# Import weight and location of the engine
 weight_engine = database_connector.load_value("engine_weight")
 spanwise_location_engine = database_connector.load_value("engine_spanwise_location")
 
-#Import the weight of the wing and fuel (Class II)
+# Import the weight of the wing and fuel (Class II)
 weight_wing = database_connector.load_value("wing_weight")
 weight_fuel = database_connector.load_value("fuel_max")
 
 
-
-#Define the lift distribution
+# Define the lift distribution
 def lift_distribution(y):
-    return xflr5.lift_coef_function_0(y)* 0.5 * density * velocity^2 * surface_area
+    return xflr5.lift_coef_function_0(y) * 0.5 * density * velocity ^ 2 * surface_area
 
-#Calculate the final force distribution
+
+# Calculate the final force distribution
 def final_force_distribution(y):
-    #if  spanwise_location_engine < y:
+    # if  spanwise_location_engine < y:
     return lift_distribution(y) - weight_wing - weight_fuel
-    #elif y == spanwise_location_engine:
+    # elif y == spanwise_location_engine:
     #    return lift_distribution(y) - weight_wing - weight_fuel - weight_engine
 
-#Integrate to shear function
+
+# Integrate to shear function
 def shear_force_function(y):
-    return 
-
-
+    return
--- InertialLoadingCalculator/force_moment_diagram_maker.py
+++ InertialLoadingCalculator/force_moment_diagram_maker.py
@@ -1,2 +1,19 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+# import Importer.xflr5 as aerodynamic_data
+# import numpy as np
 
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+# ax.plot(aerodynamic_data.chord_function.x, np.zeros(aerodynamic_data.chord_function.x.shape))
+# ax.plot(aerodynamic_data.chord_function.x, np.zeros(aerodynamic_data.chord_function.x.shape), aerodynamic_data.lift_coef_function_10.y,
+#         label='lift coef 10')
+# ax.plot(aerodynamic_data.chord_function.x, np.zeros(aerodynamic_data.chord_function.x.shape), aerodynamic_data.lift_coef_function_0.y,
+#         label='lift coef 0')
+# ax.plot(aerodynamic_data.chord_function.x, aerodynamic_data.drag_induced_function_0.y, np.zeros(aerodynamic_data.chord_function.x.shape),
+#         label='drag induced 0')
+# ax.plot(aerodynamic_data.chord_function.x, aerodynamic_data.drag_induced_function_10.y, np.zeros(aerodynamic_data.chord_function.x.shape),
+#         label='drag induced 10')
+plt.legend()
+plt.show()
