215bc9a0051e28a24be2afe528c5646934a9bf9c
Lorenzo Onorato <lollonorato@gmail.com>
2020-11-19 16:08:51 +0100

    Wingbox cornerpoints updater and spor position functions completed

--- Twist/twist_calc.py
+++ Twist/twist_calc.py
@@ -18,7 +18,7 @@ wingbox_points = database_connector.load_value()
 b1 = 
 
 # Matrix of the shear flow and change of angle. 1st variable is q1 second is q2 third is dThetha/dy.
-"""Niet voor Watjes"""
+
 def change_of_twist_angle():
 
 
@@ -31,6 +31,29 @@ wingbox_corner_points = database_connector.load_wingbox_value("wingbox_corner_po
 wingbox_centroid_location = database_connector.load_wingbox_value("wingbox_centroid_location")
 
 def spar_positions(wingbox_corner_points, wingbox_centroid_location):
+    # x position of the spar
+    x_spar = wingbox_centroid_location[0]
+
+    # y position of the spar
+    y_top = min(wingbox_corner_points[0][1], wingbox_corner_points[1][1])
+
+    slope_bottom_surface = (wingbox_corner_points[3][1] - wingbox_corner_points[2][1]) / \
+                           (wingbox_corner_points[3][0] - wingbox_corner_points[2][0])
+    y_bottom = x_spar * slope_bottom_surface
+
+    return [(x_spar, y_top), (x_spar, y_bottom)]
+
+
+def rewriting_wingbox_corner_points(wingbox_corner_points, spar_positions):
+    """Making a new wingbox list with the spar's points inserted in clockwise order"""
+
+    updated_wingbox_coordinates = [wingbox_corner_points[0],spar_positions[0], wingbox_corner_points[1]
+        , wingbox_corner_points[2],spar_positions[1], wingbox_corner_points[3]]
+
+    database_connector.save_wingbox_value("wingbox_corner_points_with_spar", updated_wingbox_coordinates)
+    database_connector.commit_to_database()
+
+    return
 
 
 
