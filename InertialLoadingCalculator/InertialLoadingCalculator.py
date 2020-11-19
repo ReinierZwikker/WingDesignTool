bce79ab0389ebb0b5015a214997b9d34d53a0e9a
Reinier Zwikker <reinier@zwikker.eu>
2020-11-19 16:05:07 +0100

    Working on integrals

--- InertialLoadingCalculator/InertialLoadingCalculator.py
+++ InertialLoadingCalculator/InertialLoadingCalculator.py
@@ -24,7 +24,7 @@ outer_diameter = database_connector.load_value("df,outer")
 radius_fuselage = outer_diameter / 2
 surface_area = database_connector.load_value("surface_area")
 
-global_length_step = 1  # [m]
+global_length_step = 0.1  # [m]
 
 # Define the flight conditions
 test_velocity = 50.0  # m/s
@@ -46,14 +46,16 @@ def lift_distribution(y, density, velocity):
 
 # Calculate the final force distribution
 def z_final_force_distribution(y, length_step, density, velocity):
-    return lift_distribution(y, density, velocity) - weight_wing / (wing_span / (2 * length_step)) - weight_fuel / (wing_span / (2 * length_step))
+    # return lift_distribution(y, density, velocity) - weight_wing / (wing_span / (2 * length_step)) - weight_fuel / (wing_span / (2 * length_step))
+    return 1
 
 
 def add_engine_component(func, y, moment_arm):
-    if y < spanwise_location_engine:
-        return func + moment_arm * weight_engine
-    else:
-        return func
+    # if y < spanwise_location_engine:
+    #     return func + moment_arm * weight_engine
+    # else:
+    #     return func
+    return 0
 
 
 # Integrate to shear function using scipy
@@ -115,15 +117,15 @@ total_time_taken = 0
 for spanwise_location in spanwise_locations_list:
     start_time_2 = time.time()
     z_force_data.append(z_final_force_distribution(spanwise_location, global_length_step, test_density, test_velocity))
-    x_shear_force_data.append(add_engine_component(x_shear_force_function_trapz(spanwise_location, wing_span/2, global_length_step, test_density, test_velocity), spanwise_location, 1))
+    x_shear_force_data.append(add_engine_component(x_shear_force_function_scipy(spanwise_location, wing_span/2, global_length_step, test_density, test_velocity), spanwise_location, 1))
     x_shear_time = time.time() - start_time_2
     start_time_3 = time.time()
     x_moment_data.append(add_engine_component(x_moment_function_trapz(spanwise_location, wing_span/2, global_length_step, test_density, test_velocity), spanwise_location, -(spanwise_location_engine - spanwise_location)))
     x_moment_time = time.time() - start_time_3
     print(f"{spanwise_location:.2f} / {wing_span / 2:.2f}:")
-    print(f"Shear: {x_shear_time:.2f} s, Moment: {x_moment_time:.2f} s, Time left approx: {(x_shear_time + x_moment_time) * wing_span / (2 * global_length_step) - total_time_taken:.0f} s\n")
+    print(f"Shear: {x_shear_time*1000:.5f} ms, Moment: {x_moment_time*1000:.5f} ms\n")
     total_time_taken += x_shear_time + x_moment_time
-print(f"total time: {(time.time() - start_time_1)/60} min")
+print(f"total time: {(time.time() - start_time_1)/60:.3f} min")
 print(f"shear LUT length: {len(x_shear_force_function_LUT)}, moment LUT length: {len(x_moment_function_LUT)}")
 # TODO Very slow from engine spanwise position? Kinda fixed
 
