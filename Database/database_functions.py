22e0145815b43b820ccb48ac8603aaef0ea6b238
Reinier Zwikker <45265447+ReinierZwikker@users.noreply.github.com>
2020-11-19 16:11:03 +0100

    Added new features to database connecter (#6)
    * Added database features
     - Database (+Backup)
     - Loading and saving functions
    Added program flowchart
    
    * Revert "Added database features  - Database (+Backup)  - Loading and saving functions Added program flowchart"
    
    This reverts commit 3c49b68f
    
    * Added database features
     - Database (+Backup)
     - Loading and saving functions
     Added program flowchart
    
    * Added feature to add to wingbox database

--- Database/database_functions.py
+++ Database/database_functions.py
@@ -36,17 +36,26 @@ class DatabaseConnector:
     def save_value(self, name, value):
         self.__database_dict[name] = value
 
+    def save_wingbox_value(self, name, value):
+        self.__wingbox_dict[name] = value
+
     def commit_to_database(self):
         with open(self.__database_file + str(2), "w") as databackup:
             json.dump(self.__database_dict, databackup)
         with open(self.__database_file, "w") as database:
             json.dump(self.__database_dict, database, indent=4)
 
+    def commit_to_wingbox_definition(self):
+        with open(self.__wingbox_file + str(2), "w") as databackup:
+            json.dump(self.__wingbox_dict, databackup)
+        with open(self.__wingbox_file, "w") as wingbox:
+            json.dump(self.__wingbox_dict, wingbox, indent=4)
+
     def preview_database(self):
         for element in self.__database_dict:
             print(f"{element}: {self.__database_dict[element]}")
 
-    def cleanup_database(self):
+    def _cleanup_database(self):
         if input("Are you sure you want to clean up the database? (Y/n)") == "Y":
             for element in self.__database_dict:
                 self.__database_dict[element.lower()] = self.__database_dict.pop(element)
