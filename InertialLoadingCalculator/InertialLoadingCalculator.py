1c9ea79335b34c73b1ef529b7421d568526d87dc
reinier zwikker <reinier@zwikker.eu>
2020-11-23 15:23:59 +0100

    Moved integration functions to Integrator.py Added drag calculations (still needs to be checked)

--- InertialLoadingCalculator/InertialLoadingCalculator.py
+++ InertialLoadingCalculator/InertialLoadingCalculator.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy import integrate
 import time
 import matplotlib.pyplot as plt
+from Integrator import Integration
 
 try:
     from Integrator import Integration
@@ -25,8 +26,8 @@ outer_diameter = database_connector.load_value("df,outer")
 radius_fuselage = outer_diameter / 2
 surface_area = database_connector.load_value("surface_area")
 
-global_length_step = 0.01  # [m]
-LUT_rounding = 1
+global_length_step = 0.1  # [m]
+LUT_rounding = 10  # digits
 
 # Define the flight conditions
 test_velocity = 150.0  # m/s
@@ -35,110 +36,149 @@ test_density = 1.225  # kg/m^2
 # Import weight and location of the engine
 weight_engine = database_connector.load_value("engine_weight")
 spanwise_location_engine = database_connector.load_value("engine_spanwise_location")
+engine_weight_width = 0.1
 
 # Import the weight of the wing and fuel (Class II)
 weight_wing = database_connector.load_value("wing_weight")
 weight_fuel = database_connector.load_value("fuel_max")
 
+# Defining where the fueltanks should be
+fuel_tank_start = 0 * (wing_span / 2)
+fuel_tank_engine_stop = spanwise_location_engine - 0.75
+fuel_tank_engine_start = spanwise_location_engine + 0.75
+fuel_tank_stop = 0.9 * (wing_span / 2)
+fuel_tank_length = (fuel_tank_engine_stop - fuel_tank_start) + (fuel_tank_stop - fuel_tank_engine_start)
 
-# Define the lift distribution
+# Import values for the drag estimation
+thickness_to_chord_ratio = database_connector.load_value("thickness_to_chord_ratio")
+cd_0 = database_connector.load_value("cd0")
+
+
+# Define the lift and drag distribution
 def lift_distribution(y, length_step, density, velocity):
     return aerodynamic_data.lift_coef_function_10(y) * 0.5 * density * (velocity ** 2) * (surface_area / (wing_span / (2 * length_step)))
 
 
-print(aerodynamic_data.lift_coef_function_10(5))
-print(lift_distribution(5, global_length_step, test_density, test_velocity))
-print(weight_wing / (wing_span / (2 * global_length_step)))
-print(weight_fuel / (wing_span / (2 * global_length_step)))
+def drag_distribution(y, length_step, density, velocity):
+    # TODO check if drag_induced is right
+    return (aerodynamic_data.drag_induced_function_10(y) + cd_0) * 0.5 * density * (velocity ** 2) * aerodynamic_data.chord_function(y) * \
+           thickness_to_chord_ratio * length_step
+
+
+print(drag_distribution(10, global_length_step, test_density, test_velocity))
 
 
 # Calculate the final force distribution
 def z_final_force_distribution(y, length_step, density, velocity):
-    return lift_distribution(y, length_step, density, velocity) - weight_wing / (wing_span / (2 * length_step)) - weight_fuel / (wing_span / (2 * length_step))
+    value = lift_distribution(y, length_step, density, velocity) - weight_wing / (wing_span / (2 * length_step))
+    if (fuel_tank_start < y < fuel_tank_engine_stop) or (fuel_tank_engine_start < y < fuel_tank_stop):
+        value -= weight_fuel / (fuel_tank_length / length_step)
+    if spanwise_location_engine - (engine_weight_width / 2) < y < spanwise_location_engine + (engine_weight_width / 2):
+        value -= weight_engine / (engine_weight_width / length_step)
+    return value
 
 
+# TODO This is very low?
+# TODO Add contribution due to thrust
+def x_final_force_distribution(y, length_step, density, velocity):
+    return drag_distribution(y, length_step, density, velocity)
+
+
+# Adding the point load of the engine after integrating (DEPRECATED)
 def add_engine_component(func, y, moment_arm):
-    # if y < spanwise_location_engine:
-    #     return func + moment_arm * weight_engine
-    # else:
-    #     return func
-    return func
+    if y < spanwise_location_engine:
+        return func + moment_arm * weight_engine
+    else:
+        return func
 
 
-def trapz_area(prev_value, current_value, length_step):
-    return prev_value + 0.5 * (current_value - prev_value) * length_step
-
-
-x_shear_force_function_LUT = {}
-x_moment_function_LUT = {}
-
-
-# Integrate using trapezoid rule
-def x_shear_force_function_trapz(start, stop, length_step, density, velocity):
-    integral_value = 0
-    for spanwise_location in np.arange(start, stop, length_step):
-        if round(spanwise_location, LUT_rounding) in x_shear_force_function_LUT:
-            integral_value += x_shear_force_function_LUT[round(spanwise_location, LUT_rounding)]
-        else:
-            prev_value = z_final_force_distribution(spanwise_location - length_step, length_step, density, velocity)
-            current_value = z_final_force_distribution(spanwise_location, length_step, density, velocity)
-            value = trapz_area(prev_value, current_value, length_step)
-            integral_value += value
-            x_shear_force_function_LUT[round(spanwise_location, LUT_rounding)] = value
-    return - integral_value
-
-
-# Integrate using trapezoid rule
-def x_moment_function_trapz(start, stop, length_step, density, velocity):
-    integral_value = 0
-    for spanwise_location in np.arange(start, stop, length_step):
-        if round(spanwise_location, LUT_rounding) in x_moment_function_LUT:
-            integral_value += x_moment_function_LUT[round(spanwise_location, LUT_rounding)]
-        else:
-            prev_value = x_shear_force_function_trapz(spanwise_location - length_step, stop, length_step, density, velocity)
-            current_value = x_shear_force_function_trapz(spanwise_location, stop, length_step, density, velocity)
-            value = trapz_area(prev_value, current_value, length_step)
-            integral_value += value
-            x_moment_function_LUT[round(spanwise_location, LUT_rounding)] = value
-    return - integral_value
-
-
+# Spanwise locations to move through for graphs
 spanwise_locations_list = np.arange(radius_fuselage, wing_span / 2, global_length_step)
 
+"""
+Sign-convention:
+x: towards nose
+y: towards tip
+z: upwards
+"""
+
+
+# Setting up the integrals
+x_shear_integral = Integration(z_final_force_distribution, LUT_rounding)
+x_moment_integral = Integration(x_shear_integral.get_value, LUT_rounding)
+
+z_shear_integral = Integration(x_final_force_distribution, LUT_rounding)
+z_moment_integral = Integration(z_shear_integral.get_value, LUT_rounding)
+
+
+# Data list for the results
 z_force_data = []
 x_shear_force_data = []
 x_moment_data = []
-start_time_1 = time.time()
-total_time_taken = 0
-for spanwise_location in spanwise_locations_list:
-    start_time_2 = time.time()
-    z_force_data.append(z_final_force_distribution(spanwise_location, global_length_step, test_density, test_velocity))
-    x_shear_force_data.append(
-        add_engine_component(x_shear_force_function_trapz(spanwise_location, wing_span / 2, global_length_step, test_density, test_velocity), spanwise_location,
-                             1))
-    x_shear_time = time.time() - start_time_2
-    start_time_3 = time.time()
-    x_moment_data.append(
-        add_engine_component(x_moment_function_trapz(spanwise_location, wing_span / 2, global_length_step, test_density, test_velocity), spanwise_location,
-                             -(spanwise_location_engine - spanwise_location)))
-    x_moment_time = time.time() - start_time_3
-    print(f"{spanwise_location:.2f} / {wing_span / 2:.2f}:")
-    print(f"Shear: {x_shear_time * 1000:.5f} ms, Moment: {x_moment_time * 1000:.5f} ms\n")
-    total_time_taken += x_shear_time + x_moment_time
-print(f"total time: {(time.time() - start_time_1) / 60:.3f} min")
-print(f"shear LUT length: {len(x_shear_force_function_LUT)}, moment LUT length: {len(x_moment_function_LUT)}")
-# TODO Very slow from engine spanwise position? Kinda fixed
 
-plt.subplot(221)
+x_force_data = []
+z_shear_force_data = []
+z_moment_data = []
+
+# Time keeping
+start_time_1 = time.time()
+
+print("Calculating shear and moment.\nIntegrating... (this can take a while)")
+
+# For every spanwise location, first integrate to shear then integrate to moment
+# TODO check signs
+for spanwise_location in spanwise_locations_list:
+    z_force_data.append(z_final_force_distribution(spanwise_location, global_length_step, test_density, test_velocity))
+    x_force_data.append(x_final_force_distribution(spanwise_location, global_length_step, test_density, test_velocity))
+    # Old way using added point loads:
+    # x_shear_force_data.append(
+    #     add_engine_component(-x_shear_integral.integrate(spanwise_location, wing_span / 2, global_length_step, test_density, test_velocity),
+    #                          spanwise_location,
+    #                          1))
+    # x_moment_data.append(
+    #     add_engine_component(x_moment_integral.integrate(spanwise_location, wing_span / 2, global_length_step, test_density, test_velocity),
+    #                          spanwise_location,
+    #                          -(spanwise_location_engine - spanwise_location)))
+
+    x_shear_force_data.append(-x_shear_integral.integrate(spanwise_location, wing_span / 2, global_length_step, test_density, test_velocity))
+    x_moment_data.append(x_moment_integral.integrate(spanwise_location, wing_span / 2, global_length_step, test_density, test_velocity))
+
+    z_shear_force_data.append(-z_shear_integral.integrate(spanwise_location, wing_span / 2, global_length_step, test_density, test_velocity))
+    z_moment_data.append(z_moment_integral.integrate(spanwise_location, wing_span / 2, global_length_step, test_density, test_velocity))
+
+# More time keeping & printing
+print(f"total time: {(time.time() - start_time_1) / 60:.3f} min")
+print(f"LUT lengths: {len(x_shear_integral.LUT)}, {len(x_moment_integral.LUT)}, {len(z_shear_integral.LUT)}, {len(z_moment_integral.LUT)}")
+
+# Make plots for the x direction
+plt.subplot(321)
+plt.title("lift and weight")
 plt.plot(spanwise_locations_list, z_force_data, label="Force")
-plt.ylabel("Applied Force in z (N)")
+plt.ylabel("Applied Force in z (N)\nWithout point forces")
 plt.xlabel("Wing location (m)")
-plt.subplot(223)
+plt.subplot(323)
 plt.plot(spanwise_locations_list, x_shear_force_data, label="Shear")
 plt.ylabel("Shear force in x (N)")
 plt.xlabel("Wing location (m)")
-plt.subplot(224)
+plt.subplot(325)
 plt.plot(spanwise_locations_list, x_moment_data, label="Moment")
 plt.ylabel("Moment in x (Nm)")
 plt.xlabel("Wing location (m)")
+
+# Make plots for the z direction
+plt.subplot(322)
+plt.title("drag and (thrust)")
+plt.plot(spanwise_locations_list, x_force_data, label="Force")
+plt.ylabel("Applied Force in x (N)\nWithout point forces")
+plt.xlabel("Wing location (m)")
+plt.subplot(324)
+plt.plot(spanwise_locations_list, z_shear_force_data, label="Shear")
+plt.ylabel("Shear force in z (N)")
+plt.xlabel("Wing location (m)")
+plt.subplot(326)
+plt.plot(spanwise_locations_list, z_moment_data, label="Moment")
+plt.ylabel("Moment in z (Nm)")
+plt.xlabel("Wing location (m)")
+
+# show and pop plots
 plt.show()
--- Integrator.py
+++ Integrator.py
@@ -9,18 +9,26 @@ class Integration:
 
     def __init__(self, function, LUT_rounding):
         self.LUT = {}
-        self.function = function
-        self.LUT_rounding = LUT_rounding
+        self.__function = function
+        self.__LUT_rounding = LUT_rounding
 
     def integrate(self, start, stop, length_step, *args):
         integral_value = 0
         for location in np.arange(start, stop, length_step):
-            if round(location, self.LUT_rounding) in self.LUT:
-                integral_value += self.LUT[round(location, self.LUT_rounding)]
+            if round(location, self.__LUT_rounding) in self.LUT:
+                integral_value += self.LUT[round(location, self.__LUT_rounding)]
             else:
-                prev_value = self.function(location - length_step, length_step, *args)
-                current_value = self.function(location, length_step, *args)
+                # print("Cannot find value in LUT!")
+                prev_value = self.__function(location - length_step, length_step, *args)
+                current_value = self.__function(location, length_step, *args)
                 value = trapz_area(prev_value, current_value, length_step)
                 integral_value += value
-                self.LUT[round(location, self.LUT_rounding)] = value
-        return -integral_value
+                self.LUT[round(location, self.__LUT_rounding)] = value
+        return integral_value
+
+    def get_value(self, location, *args):
+        if round(location, self.__LUT_rounding) in self.LUT:
+            return self.LUT[round(location, self.__LUT_rounding)]
+        else:
+            # print("integrating for other function")
+            return self.integrate(location, *args)
