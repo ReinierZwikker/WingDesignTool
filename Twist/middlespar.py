4bee2bc2101f1eb006a85e2a71313f680287d192
Gidzo16 <groeneveldgideon@gmail.com>
2020-11-24 13:35:05 +0100

    Angle of twist finished, but weird value

---
+++ Twist/middlespar.py
@@ -0,0 +1,43 @@
+import numpy as np
+import scipy as sp
+
+try:
+    from Database.database_functions import DatabaseConnector
+except ModuleNotFoundError:
+    import sys
+    from os import path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+    from Database.database_functions import DatabaseConnector
+
+database_connector = DatabaseConnector()
+
+wingbox_corner_points = database_connector.load_wingbox_value("wingbox_corner_points")
+wingbox_centroid_location = [0.37216, 0.01494] #database_connector.load_wingbox_value("wingbox_centroid_location")
+
+
+def spar_positions():
+    # x position of the spar
+    x_spar = wingbox_centroid_location[0]
+
+    slope_top_surface = (wingbox_corner_points[1][1] - wingbox_corner_points[0][1])/ (wingbox_corner_points[1][0] - wingbox_corner_points[0][0])
+    y_top = (x_spar - wingbox_corner_points[0][0]) * slope_top_surface + wingbox_corner_points[0][1]
+
+    slope_bottom_surface = (wingbox_corner_points[3][1] - wingbox_corner_points[2][1])/ (wingbox_corner_points[3][0] - wingbox_corner_points[2][0])
+    y_bottom = (x_spar - wingbox_corner_points[3][0]) * slope_bottom_surface + wingbox_corner_points[3][1]
+    
+    return (x_spar, y_top), (x_spar, y_bottom)
+
+
+def rewriting_wingbox_corner_points():
+    """Making a new wingbox list with the spar's points inserted in clockwise order"""
+    mid_spar = spar_positions()
+    updated_wingbox_coordinates = [wingbox_corner_points[0], mid_spar[0], wingbox_corner_points[1]
+        , wingbox_corner_points[2], mid_spar[1], wingbox_corner_points[3]]
+
+    database_connector.save_wingbox_value("wingbox_points", updated_wingbox_coordinates)
+    database_connector.commit_to_wingbox_definition()
+
+    return
+
+rewriting_wingbox_corner_points()
--- Twist/twist_calc.py
+++ Twist/twist_calc.py
@@ -1,10 +1,11 @@
 import numpy as np
 import scipy as sp
+from scipy import integrate
+from math import *
 
 try:
     from Database.database_functions import DatabaseConnector
     from WingData.chord_function import chord_function
-    # from WingboxCreator.wingbox_creator import wingbox_area, wingbox_line
 except ModuleNotFoundError:
     import sys
     from os import path
@@ -12,70 +13,35 @@ except ModuleNotFoundError:
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
     from Database.database_functions import DatabaseConnector
     from WingData.chord_function import chord_function
-    # from WingboxCreator.wingbox_creator import wingbox_area, wingbox_line
 
 database_connector = DatabaseConnector()
 
-G = database_connector.load_wingbox_value("shear_modulus_Pa")
-t1 = database_connector.load_wingbox_value("...")
-t3 = database_connector.load_wingbox_value("...")
-wingbox_points = database_connector.load_wingbox_value("wingbox_corner_points")
-b1 = wingbox_points[0][1] - wingbox_points[3][1]
-b2 = wingbox_points[1][1] - wingbox_points[2][1]
-b3 = 0
-a1 = 0
-a2 = 0
-c1 = 0
-c2 = 0
-A1 = 0.5 * (b1+b3)(wingbox_points[.][.]-wingbox_points[0][0])
-A2 = 0.5 * (b2+b3)(wingbox_points[1][0]-wingbox_points[.][.])
+G = 26000000000 #database_connector.load_wingbox_value("shear_modulus_Pa")
+t1 = 0.003 #database_connector.load_wingbox_value("...")
+t3 = 0.003 #database_connector.load_wingbox_value("...")
+
+wingbox_points = database_connector.load_wingbox_value("wingbox_points")
+
+b_one = (wingbox_points[0][1] - wingbox_points[5][1])
+b_two = (wingbox_points[2][1] - wingbox_points[3][1])
+b_three = (wingbox_points[1][1] - wingbox_points[4][1])
+a_one = sqrt((wingbox_points[0][1]-wingbox_points[1][1])**2 + (wingbox_points[1][0]-wingbox_points[0][0])**2)
+a_two = sqrt((wingbox_points[4][1]-wingbox_points[5][1])**2 + (wingbox_points[4][0]-wingbox_points[5][0])**2)
+c_one = sqrt((wingbox_points[1][1]-wingbox_points[2][1])**2 + (wingbox_points[1][0]-wingbox_points[2][0])**2)
+c_two = sqrt((wingbox_points[3][1]-wingbox_points[4][1])**2 + (wingbox_points[3][0]-wingbox_points[4][0])**2)
+Area_first = 0.5 * (b_one+b_three)*(wingbox_points[1][0]-wingbox_points[0][0])
+Area_second = 0.5 * (b_two+b_three)*(wingbox_points[2][0]-wingbox_points[1][0])
 
 # Matrix of the shear flow and change of angle. 1st variable is q1 second is q2 third is dThetha/dy.
 def change_of_twist_angle(y):
     chord = chord_function(y)
-    matrix = np.array([[2*A1*chord*chord, 2*A2*chord*chord, 0], 
-                [(((((b1+a1+a2)*chord)/t1)+(b3*chord/t3))/2*A1*chord*chord*G), -((b3*chord/t3)/2*A1*chord*chord*G), -1], 
-                [-((b3*chord/t3)/2*A2*chord*chord*G), (((((b2+c1+c2)*chord/t1)+(b3*chord/t3))/2*A2*chord*chord*G), -1]])
-    solution_vector = np.array([TorquePlaceHolder(), 0, 0])
-    return np.linalg.solve(matrix, solution_vector)
+    matrix = np.array([[2*Area_first*chord*chord, 2*Area_second*chord*chord, 0], 
+                [(((((b_one+a_one+a_two)*chord)/t1)+(b_three*chord/t3))/2*Area_first*chord*chord*G), -1*((b_three*chord/t3)/2*Area_first*chord*chord*G), -1], 
+                [-1*((b_three*chord/t3)/2*Area_second*chord*chord*G), (((((b_two+c_one+c_two)*chord/t1)+(b_three*chord/t3))/2*Area_second*chord*chord*G)), -1]])
+    solution_vector = np.array([1, 0, 0]) #TorquePlaceHolder() in first array slot
+    q1, q2, dThetha = np.linalg.solve(matrix, solution_vector)
+    return dThetha
 
-
-# Height of the middle spar based on imported value
-wingbox_corner_points = database_connector.load_wingbox_value("wingbox_corner_points")
-wingbox_centroid_location = database_connector.load_wingbox_value("wingbox_centroid_location")
-
-
-def spar_positions(wingbox_corner_points, wingbox_centroid_location):
-    # x position of the spar
-    x_spar = wingbox_centroid_location[0]
-
-    # y position of the spar
-    y_top = min(wingbox_corner_points[0][1], wingbox_corner_points[1][1])
-
-    slope_bottom_surface = (wingbox_corner_points[3][1] - wingbox_corner_points[2][1]) / \
-                           (wingbox_corner_points[3][0] - wingbox_corner_points[2][0])
-    y_bottom = x_spar * slope_bottom_surface + wingbox_corner_points[3][1]
-
-    return [(x_spar, y_top), (x_spar, y_bottom)]
-
-
-def rewriting_wingbox_corner_points(wingbox_corner_points, spar_positions):
-    """Making a new wingbox list with the spar's points inserted in clockwise order"""
-
-    updated_wingbox_coordinates = [wingbox_corner_points[0], spar_positions[0], wingbox_corner_points[1]
-        , wingbox_corner_points[2], spar_positions[1], wingbox_corner_points[3]]
-
-    database_connector.save_wingbox_value("wingbox_corner_points_with_spar", updated_wingbox_coordinates)
-    database_connector.commit_to_wingbox_definition()
-
-    return
-
-
-# Torsional constant calculation
-def J_y(d_theta_d_y, G, T_y):
-    J_y = (T_y) / (d_theta_d_y * G)
-    return J_y
-
-# def J_value(J_y, y_position):
-# J_@y = (J_y * y_position)
-# return J_@y
+def twist_angle():
+    estimate,error = sp.integrate.quad(change_of_twist_angle, 0, database_connector.load_value("halfspan"))
+    return estimate
