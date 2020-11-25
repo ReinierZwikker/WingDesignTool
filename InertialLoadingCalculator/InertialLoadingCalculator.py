830af25220cf877b002f964ff6a5a2368faf4be0
Reinier Zwikker <reinier@zwikker.eu>
2020-11-25 15:19:05 +0100

    Fixed step dependency bug

--- InertialLoadingCalculator/InertialLoadingCalculator.py
+++ InertialLoadingCalculator/InertialLoadingCalculator.py
@@ -10,6 +10,7 @@ try:
 except ModuleNotFoundError:
     import sys
     from os import path
+
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
     import Importer.xflr5 as aerodynamic_data
     from Database.database_functions import DatabaseConnector
@@ -26,28 +27,27 @@ root_chord = database_connector.load_value("root_chord")
 tip_chord = database_connector.load_value("tip_chord")
 taper_ratio = database_connector.load_value("taper_ratio")
 chord_at_engine_location = database_connector.load_value("engine_chord")
-moment_arm_engine = 0.7* chord_at_engine_location
+moment_arm_engine = 0.7 * chord_at_engine_location
 radius_engine = database_connector.load_value("d_engine")
 moment_arm_thrust = 1.5 * radius_engine
 
-global_length_step = 1  # [m]
-LUT_rounding = 10  # digits
+global_length_step = 0.1  # [m]
 
 # Define the flight conditions
-test_velocity = 150.0  # m/s
+test_velocity = 100.0  # m/s
 test_density = 1.225  # kg/m^2
 
 # Import weight and location of the engine
 weight_engine = database_connector.load_value("engine_weight")
 spanwise_location_engine = database_connector.load_value("engine_spanwise_location")
-engine_weight_width = 0.1
+engine_weight_width = 1
 engine_thrust = database_connector.load_value("engine_max_thrust")
 
 # Import the weight of the wing and fuel (Class II)
 weight_wing = database_connector.load_value("wing_weight")
 weight_fuel = database_connector.load_value("fuel_max")
 
-# Defining where the fueltanks should be
+# Defining where the fuel tanks should be
 fuel_tank_start = 0 * (wing_span / 2)
 fuel_tank_engine_stop = spanwise_location_engine - 0.75
 fuel_tank_engine_start = spanwise_location_engine + 0.75
@@ -60,29 +60,30 @@ cd_0 = database_connector.load_value("cd0")
 
 include_fuel_tanks = True
 include_engine = True
+fuel_tank_level = 0.5  # level of the fuel tanks from 0 to 1
 
 
 # Define the lift and drag distribution
 def lift_distribution(y, length_step, density, velocity):
-    return aerodynamic_data.lift_coef_function_10(y) * 0.5 * density * (velocity ** 2) * aerodynamic_data.chord_function(y) * length_step
+    return aerodynamic_data.lift_coef_function_10(y) * 0.5 * density * (velocity ** 2) * aerodynamic_data.chord_function(y)
 
 
 def drag_distribution(y, length_step, density, velocity):
-    # TODO check if drag_induced is right
-    return (aerodynamic_data.drag_induced_function_10(y) + cd_0) * 0.5 * density * (velocity ** 2) * aerodynamic_data.chord_function(y) * length_step
+    return (aerodynamic_data.drag_induced_function_10(y) + cd_0) * 0.5 * density * (velocity ** 2) * aerodynamic_data.chord_function(y)
 
 
 def pitching_moment_function(y, density, velocity, length_step):
-    #0.5 rho V^2 S c
-    print(aerodynamic_data.moment_coef_function_10(y))
-    return -aerodynamic_data.moment_coef_function_10(y) * 0.5 * density * (velocity**2) *aerodynamic_data.chord_function(y) * length_step * aerodynamic_data.chord_function(y)
+    # 0.5 rho V^2 S c
+    # print(aerodynamic_data.moment_coef_function_10(y))
+    return -aerodynamic_data.moment_coef_function_10(y) * 0.5 * density * (velocity ** 2) * aerodynamic_data.chord_function(
+        y) * length_step * aerodynamic_data.chord_function(y)
 
 
 # Calculate the final force distribution
 def z_final_force_distribution(y, length_step, density, velocity):
-    value = lift_distribution(y, length_step, density, velocity) # - weight_wing / surface_area * aerodynamic_data.chord_function(y) * length_step
+    value = lift_distribution(y, length_step, density, velocity) - weight_wing / (surface_area / 2) * aerodynamic_data.chord_function(y)
     if ((fuel_tank_start < y < fuel_tank_engine_stop) or (fuel_tank_engine_start < y < fuel_tank_stop)) and include_fuel_tanks:
-        value -= weight_fuel / (fuel_tank_length / length_step)
+        value -= (weight_fuel / fuel_tank_length) * fuel_tank_level
     if spanwise_location_engine - length_step / 2 < y < spanwise_location_engine + length_step / 2 and include_engine:
         value -= weight_engine
     return value
@@ -95,14 +96,14 @@ def x_final_force_distribution(y, length_step, density, velocity):
 
 # Adding the point load of the engine after integrating
 def add_engine_moment(y, length_step, moment_arm):
-    if spanwise_location_engine + length_step/2 > y:
+    if spanwise_location_engine + length_step / 2 > y:
         return moment_arm * weight_engine
     else:
         return 0
 
 
 def add_thrust_moment(y, length_step, moment_arm):
-    if spanwise_location_engine + length_step/2 > y:
+    if spanwise_location_engine + length_step / 2 > y:
         return moment_arm * engine_thrust
     else:
         return 0
@@ -112,6 +113,10 @@ def add_thrust_moment(y, length_step, moment_arm):
 spanwise_locations_list = np.arange(radius_fuselage, wing_span / 2, global_length_step)
 # spanwise_locations_list = np.arange(0, 10, global_length_step)
 
+# spanwise_locations_list_10 = np.arange(radius_fuselage, wing_span / 2, global_length_step/10)
+# spanwise_locations_list_100 = np.arange(radius_fuselage, wing_span / 2, global_length_step/100)
+# spanwise_locations_list_1000 = np.arange(radius_fuselage, wing_span / 2, global_length_step/1000)
+
 """
 Sign-convention:
 x: towards nose
@@ -119,7 +124,6 @@ y: towards tip
 z: upwards
 counterclockwise
 """
-
 
 # Setting up the integrals
 # x_shear_integral = Integration(z_final_force_distribution, min(spanwise_locations_list), max(spanwise_locations_list))
@@ -131,9 +135,12 @@ x_moment_integral = Integration(x_shear_integral.get_value, min(spanwise_locatio
 z_shear_integral = Integration(x_final_force_distribution, min(spanwise_locations_list), max(spanwise_locations_list), flip_sign=True)
 z_moment_integral = Integration(z_shear_integral.get_value, min(spanwise_locations_list), max(spanwise_locations_list), flip_sign=True)
 
-
 # Data list for the results
 z_force_data = []
+z_force_data_10 = []
+z_force_data_100 = []
+z_force_data_1000 = []
+
 x_shear_force_data = []
 x_moment_data = []
 
@@ -149,7 +156,6 @@ start_time_1 = time.time()
 print("Calculating shear and moment.\nIntegrating... (this can take a while)")
 
 # For every spanwise location, first integrate to shear then integrate to moment
-# TODO check signs
 for spanwise_location in spanwise_locations_list:
     z_force_data.append(z_final_force_distribution(spanwise_location, global_length_step, test_density, test_velocity))
     x_force_data.append(x_final_force_distribution(spanwise_location, global_length_step, test_density, test_velocity))
@@ -160,46 +166,64 @@ for spanwise_location in spanwise_locations_list:
     z_shear_force_data.append(z_shear_integral.integrate(spanwise_location, global_length_step, test_density, test_velocity))
     z_moment_data.append(z_moment_integral.integrate(spanwise_location, global_length_step, test_density, test_velocity))
 
-    y_torsion_data.append(pitching_moment_function(spanwise_location, test_density, test_velocity, global_length_step) + add_engine_moment(spanwise_location,global_length_step, moment_arm_engine) + add_thrust_moment(spanwise_location, global_length_step, moment_arm_thrust))
+    y_torsion_data.append(
+        pitching_moment_function(spanwise_location, test_density, test_velocity, global_length_step) + add_engine_moment(spanwise_location, global_length_step,
+                                                                                                                         moment_arm_engine) + add_thrust_moment(
+            spanwise_location, global_length_step, moment_arm_thrust))
+
+
+# lift = 0
+# for spanwise_location in spanwise_locations_list_10:
+#     z_force_data_10.append(z_final_force_distribution(spanwise_location, global_length_step/10, test_density, test_velocity))
+#
+# for spanwise_location in spanwise_locations_list_100:
+#     z_force_data_100.append(z_final_force_distribution(spanwise_location, global_length_step/100, test_density, test_velocity))
+#
+# for spanwise_location in spanwise_locations_list_1000:
+#     lift += z_final_force_distribution(spanwise_location, global_length_step, test_density, test_velocity)
+#     z_force_data_1000.append(z_final_force_distribution(spanwise_location, global_length_step/1000, test_density, test_velocity))
+# print(2*lift/1000)
+
 
 # More time keeping & printing
 print(f"total time: {(time.time() - start_time_1) / 60:.3f} min")
 print(f"LUT lengths: {len(x_shear_integral.LUT)}, {len(x_moment_integral.LUT)}, {len(z_shear_integral.LUT)}, {len(z_moment_integral.LUT)}")
 
 # Make plots for the x direction
+plt.subplots_adjust(left=0.07, bottom=0.07, right=0.97, top=0.95, wspace=0.10, hspace=0.27)
 plt.subplot(421)
-plt.title("lift and weight")
-plt.plot(spanwise_locations_list, z_force_data, label="Force")
-plt.ylabel("Applied Force in z (N)\nWithout point forces")
-plt.xlabel("Wing location (m)")
+plt.title("z: Lift and Weight")
+plt.plot(spanwise_locations_list, [x/1000 for x in z_force_data], label="Force")
+plt.ylabel("Applied Force in z (kN)")
 plt.subplot(423)
-plt.plot(spanwise_locations_list, x_shear_force_data, label="Shear")
-plt.ylabel("Shear force in x (N)")
-plt.xlabel("Wing location (m)")
+plt.title("x: Shear due to Lift and Weight")
+plt.plot(spanwise_locations_list, [x/1000 for x in x_shear_force_data], label="Shear")
+plt.ylabel("Shear force in x (kN)")
 plt.subplot(425)
-plt.plot(spanwise_locations_list, x_moment_data, label="Moment")
-plt.ylabel("Moment in x (Nm)")
-plt.xlabel("Wing location (m)")
+plt.title("x: Moment due to Lift and Weight")
+plt.plot(spanwise_locations_list, [x/1000 for x in x_moment_data], label="Moment")
+plt.ylabel("Moment in x (kNm)")
 
 # Make plots for the z direction
 plt.subplot(422)
-plt.title("drag and (thrust)")
-plt.plot(spanwise_locations_list, x_force_data, label="Force")
-plt.ylabel("Applied Force in x (N)\nWithout point forces")
-plt.xlabel("Wing location (m)")
+plt.title("x: Drag and Thrust")
+plt.plot(spanwise_locations_list, [x/1000 for x in x_force_data], label="Force")
+plt.ylabel("Applied Force in x (kN)")
 plt.subplot(424)
-plt.plot(spanwise_locations_list, z_shear_force_data, label="Shear")
-plt.ylabel("Shear force in z (N)")
-plt.xlabel("Wing location (m)")
+plt.title("z: Shear due to Drag and Thrust")
+plt.plot(spanwise_locations_list, [x/1000 for x in z_shear_force_data], label="Shear")
+plt.ylabel("Shear force in z (kN)")
 plt.subplot(426)
-plt.plot(spanwise_locations_list, z_moment_data, label="Moment")
-plt.ylabel("Moment in z (Nm)")
+plt.title("z: Moment due to Drag and Thrust")
+plt.plot(spanwise_locations_list, [x/1000 for x in z_moment_data], label="Moment")
+plt.ylabel("Moment in z (kNm)")
 plt.xlabel("Wing location (m)")
 
-#Make plots for the u direction (torsion)
+# Make plots for the u direction (torsion)
 plt.subplot(427)
-plt.plot(spanwise_locations_list, y_torsion_data, label="Torsion")
-plt.ylabel("Torsion in y (Nm)")
+plt.title("u: Torsion")
+plt.plot(spanwise_locations_list, [x/1000 for x in y_torsion_data], label="Torsion")
+plt.ylabel("Torsion in y (kNm)")
 plt.xlabel("Wing location (m)")
 
 # show and pop plots
