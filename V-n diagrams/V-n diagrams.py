095f4ccea9256533a362a7eb65a40f0aee2eafdb
Lorenzo Onorato <lollonorato@gmail.com>
2020-11-26 14:32:00 +0100

    isa included

--- V-n diagrams/V-n diagrams.py
+++ V-n diagrams/V-n diagrams.py
@@ -37,6 +37,43 @@ elif n_max > 3.8:
 else:
     n_max = n_max
 
+# ISA Calculator
+def ISA_T_P_d(h):
+    end = False
+
+    # constants
+    g_0 = 9.80665  # m/s^2
+    R = 287.0  # J/kgK
+    # Sea level values:
+    T_0 = 288.15  # K
+    p_0 = 101325.0  # Pa
+    h_0 = 0
+
+    atm_layers = [0.0, 11000.0, 20000.0, 32000.0, 47000.0, 51000.0, 71000.0, 86000.0]
+    atm_layerx = 1
+    lay_coeff = [0.0, -0.0065, 0.0, +0.0010, +0.0028, 0.0, -0.0028, -0.0020]  # K/m
+
+    while not end:
+        # Layer Calculation
+        h1 = min(h, atm_layers[atm_layerx])  # altitude
+        T = T_0 + lay_coeff[atm_layerx] * (h1 - h_0)  # temperature
+        if lay_coeff[atm_layerx] == 0.0:  # pressure
+            p = p_0 * exp((-g_0 / (R * T)) * (h1 - h_0))
+        else:
+            p = p_0 * (T / T_0) ** (-g_0 / (lay_coeff[atm_layerx] * R))
+        rho = p / (R * T)  # density
+
+        if h <= atm_layers[atm_layerx]:
+            end = True
+        else:
+            T_0 = T
+            p_0 = p
+            h_0 = h1
+            atm_layerx = 1 + atm_layerx
+
+    return T, p, rho
+
+
 V_S0 = (np.sqrt((2*w)/(C_L_max_flapped*rho_0*S)))*np.sqrt(rho/rho_0) #stall speed with flaps extended.(should always be accompanied by a specification of which configuration the flaps are in (e.g. landing, take-off, etc.) EAS.
 V_S1 = (np.sqrt((2*w)/(C_L_max_clean*rho_0*S)))*np.sqrt(rho/rho_0) #stall speed with flaps retracted EAS.
 V_A = V_S1*(np.sqrt(n_max)) #manoeuvring speed
@@ -107,7 +144,6 @@ print("V_S1 =", V_S1)
 print("V_A =", V_A)
 print("V_C =", V_C)
 print("V_D =", V_D)
-
 
 
 
