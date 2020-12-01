dfe18ec1d4136083442e56f7d6985053cc8a3688
Reinier Zwikker <45265447+ReinierZwikker@users.noreply.github.com>
2020-12-01 17:03:22 +0100

    working on polar moi

--- AileronReversalCalculator/aileron_reversal_calc.py
+++ AileronReversalCalculator/aileron_reversal_calc.py
@@ -9,7 +9,7 @@ except ModuleNotFoundError:
     from Database.database_functions import DatabaseConnector
     import Importer.xflr5 as aerodynamic_data
 
-
+database_connector = DatabaseConnector()
 # assumptions:
 #   shear centre coincides with centroid
 #   two-dimensional flow
@@ -24,6 +24,11 @@ except ModuleNotFoundError:
 #   e = distance c/4 and shear center (geometry)
 #   G
 #   J
+
+
+
+inboard_aileron_amount_of_stringers = ''
+outboard_aileron_amount_of_stringers = ''
 
 def get_polar_moment_of_inertia():
     return 1
