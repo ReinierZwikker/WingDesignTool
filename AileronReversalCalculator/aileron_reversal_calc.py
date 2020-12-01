09ee6770a692224db74dbb02fa17c8175fef4e06
Reinier Zwikker <45265447+ReinierZwikker@users.noreply.github.com>
2020-12-01 16:30:41 +0100

    Added imports

--- AileronReversalCalculator/aileron_reversal_calc.py
+++ AileronReversalCalculator/aileron_reversal_calc.py
@@ -1,15 +1,29 @@
-#assumptions:
-## shear centre coincides with centroid
-## two-dimensional flow
+try:
+    from Database.database_functions import DatabaseConnector
+    import Importer.xflr5 as aerodynamic_data
+except ModuleNotFoundError:
+    import sys
+    from os import path
 
-#Needed variables
-## dCL/drareletter (XFOIL)
-## dCM0/drareletter (XFOIL)
-## density & velocity (cruise & sea level)
-## surface area (database)
-## chord @ aileron (chord function)
-## K = GJ
-## e = distance c/4 and shear center (geometry)
-## G
-## J
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+    from Database.database_functions import DatabaseConnector
+    import Importer.xflr5 as aerodynamic_data
 
+
+# assumptions:
+#   shear centre coincides with centroid
+#   two-dimensional flow
+
+# Needed variables
+#   dCL/dXi (XFOIL)
+#   dCM0/dXi (XFOIL)
+#   density & velocity (cruise & sea level)
+#   surface area (database)
+#   chord @ aileron (chord function)
+#   K = GJ
+#   e = distance c/4 and shear center (geometry)
+#   G
+#   J
+
+def get_polar_moment_of_inertia():
+    return 1
