297db0a731413a9c694024ae231c09b86e089b79
Gidzo16 <groeneveldgideon@gmail.com>
2020-12-01 17:22:22 +0100

    First buckling load

---
+++ Buckling/stringer.py
@@ -0,0 +1,18 @@
+from math import *
+
+try:
+    from Database.database_functions import DatabaseConnector
+except ModuleNotFoundError:
+    import sys
+    from os import path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+    from Database.database_functions import DatabaseConnector
+
+database_connector = DatabaseConnector()
+
+I = 0 # Moment of inertia per stringer
+E = database_connector.load_wingbox_value("stringer_modulus")
+K = 4 #May vary per section in the wingbox
+def crit_stress_stringer(L): #L is the wingbox section length
+    sigma = (K * (pi ** 2) * E * I) / (L**2)
+    return sigma
