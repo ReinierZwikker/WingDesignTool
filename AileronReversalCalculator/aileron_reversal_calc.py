99f0a4ed5b9212ad36f48ae98799bff3c344c703
dorotheruys <dorothe@beruys.be>
2020-12-01 16:11:35 +0100

    Added assumptions and needed variables

---
+++ AileronReversalCalculator/aileron_reversal_calc.py
@@ -0,0 +1,15 @@
+#assumptions:
+## shear centre coincides with centroid
+## two-dimensional flow
+
+#Needed variables
+## dCL/drareletter (XFOIL)
+## dCM0/drareletter (XFOIL)
+## density & velocity (cruise & sea level)
+## surface area (database)
+## chord @ aileron (chord function)
+## K = GJ
+## e = distance c/4 and shear center (geometry)
+## G
+## J
+
