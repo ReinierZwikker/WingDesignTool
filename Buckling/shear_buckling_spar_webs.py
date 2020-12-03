761115b7674326bbbcfada18c8a0b9da40e2df62
Lorenzo Onorato <lollonorato@gmail.com>
2020-12-03 16:27:40 +0100

    een moment alsjeblieft

--- Buckling/shear_buckling_spar_webs.py
+++ Buckling/shear_buckling_spar_webs.py
@@ -3,13 +3,13 @@
 
 try:
     from Database.database_functions import DatabaseConnector
-    from Twist.twist_calc import dtheta_multi
+    from Twist.twist_calc import dtheta_multi, lift_distribution, drag_distribution
 except ModuleNotFoundError:
     import sys
     from os import path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
     from Database.database_functions import DatabaseConnector
-    from Twist.twist_calc import dtheta_multi
+    from Twist.twist_calc import dtheta_multi, lift_distribution, drag_distribution
 
 
 database_connector = DatabaseConnector()
@@ -17,4 +17,10 @@ database_connector = DatabaseConnector()
 
 # importing form wing-box database
 t_spar = database_connector.load_wingbox_value("spar_thickness")
+
+
+# def shear_lift
+
+
+
 
