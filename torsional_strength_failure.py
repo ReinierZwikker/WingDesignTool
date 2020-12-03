fdff708dbd27d24431c1f4ff3981d96554b70ec7
BVL2000 <74250309+BVL2000@users.noreply.github.com>
2020-12-03 16:39:22 +0100

    new2.0.final.pro.max.duo.last.new

--- torsional_strength_failure.py
+++ torsional_strength_failure.py
@@ -2,34 +2,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 try:
+    from Integrator import Integration
+    #from InertialLoadingCalculator.inertial_loading_calculator import ...
     from Database.database_functions import DatabaseConnector
-    from Gust_Loading_calculations_moved import W_list, U_ref, WoS, U_ds, F_g, rho_0, V_S1, V_C_cruise_altitude, rho_altitude
 except ModuleNotFoundError:
     import sys
     from os import path
 
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+    #from InertialLoadingCalculator.inertial_loading_calculator import ...
     from Database.database_functions import DatabaseConnector
-
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-    from Database.database_functions import DatabaseConnector
-    from Gust_Loading_calculations_moved import W_list, U_ref, WoS, U_ds, F_g, rho_0, V_S1, V_C_cruise_altitude, rho_altitude
+    from Integrator import Integration
 
 database_connector = DatabaseConnector()
 
 #constants
-hb = database_connector.load_value("halfspan")
+hb = database_connector.load_value("wing_span")/2
+length_steps = 0.5
+i = 0 #location bl
+
+#lists
+bl_list = []
+string_stress_nomral_list = []
+mos_list = []
 
 #normal stress stringers due to bending
-def string_stress_normal(x, y, bl):
-    M_y =
-    M_x =
-    I_yy =
-    I_xx =
-    I_xy =
-    #b = #span location
-    # x = #max distance to centroid
-    # y = #max distance to centroid
+def string_stress_normal(bl):
+    M_y = 5*(60-bl)
+    M_x = 4*(60-bl)
+    I_yy = 20
+    I_xx = 30
+    I_xy = 20
+    x = 0 #not #max distance to centroid
+    y = 3 #not #max distance to centroid
     sigma = ((((M_x*I_yy)-(M_y*I_xy))*y)+(((M_y*I_xx)-(M_x*I_xy))*x))/((I_xx*I_yy)-(I_xy)**2)
     return sigma
 
@@ -38,23 +43,28 @@ def margin_of_safety(applied_stress):
     mos = failure_stress/applied_stress
     return mos
 
-mos = margin_of_safety(5000)
+while i <= hb:
+    applied_stress = string_stress_normal(i)
+    string_stress_nomral_list.append(applied_stress)
+    mos = margin_of_safety(applied_stress)
+    mos_list.append(mos)
+    bl_list.append(i)
+    i += length_steps
 
 #plotting-code
 #plot-settings
 #plt.ylim(-3, 3)
 fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.spines['bottom'].set_position('zero')
-y_ticks = np.arange(-3, 5, 0.5)
-x_ticks = np.arange(0, b/2, 0.5)
+y_ticks = np.arange(0, 5, 0.5)
+x_ticks = np.arange(0, hb, 5)
 plt.xticks(x_ticks)
 plt.yticks(y_ticks)
-plt.xlabel('span')
+plt.xlabel('span location')
 plt.ylabel('margin of safety')
 
 #for line 0 to A
-plt.plot(bl, mos, 'c')
+plt.plot(bl_list, mos_list, 'c')
 
 #show plot
 plt.show()
+
