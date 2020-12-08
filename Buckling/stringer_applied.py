51cebb797e1024553f16f88b11d9713c75b7ef9b
Gidzo16 <groeneveldgideon@gmail.com>
2020-12-08 16:34:28 +0100

    Cleaned up

--- Buckling/stringer_applied.py
+++ Buckling/stringer_applied.py
@@ -1,19 +1,21 @@
-from math import *
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+import pickle
 
 try:
+    from Integrator import Integration
     from Database.database_functions import DatabaseConnector
-    from IntertialLoadingCalculator.inertial_loading_calculator import drag_distribution
+    from MOIcalc.moicalc import inertia
 except ModuleNotFoundError:
     import sys
     from os import path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
     from Database.database_functions import DatabaseConnector
-    from IntertialLoadingCalculator.inertial_loading_calculator import z_final_force_distribution, lift_distribution
+    from Integrator import Integration
+    from MOIcalc.moicalc import inertia
 
 database_connector = DatabaseConnector()
-
-Ixx = database_connector.load_wingbox_value("")
-Izz = database_connector.load_wingbox_value("")
 
 try:
     with open("../InertialLoadingCalculator/data.pickle", 'rb') as file:
@@ -22,18 +24,25 @@ except FileNotFoundError:
     with open("./data.pickle", 'rb') as file:
         data = pickle.load(file)
 
-spanwise_locations_list = data[0]
-x_moment_data = data[3]
-x_moment_func = sp.interpolate.interp1d(spanwise_locations_list, x_moment_data, kind="cubic", fill_value="extrapolate")
-global_step_length = spanwise_locations_list[1] - spanwise_locations_list[0]
+y_lst = data[0]
+x_moment_lst = data[3]
+z_moment_lst = data[6]
+step = y_lst[1] - y_lst[0]
+b = database_connector.load_value("wing_span")/2
+
+x_moment = sp.interpolate.interp1d(y_lst, x_moment_lst, kind="cubic", fill_value="extrapolate")
+z_moment = sp.interpolate.interp1d(y_lst, z_moment_lst, kind="cubic", fill_value="extrapolate")
 
 
-def moment_lift():
-    lift_distribution
-
-def moment_drag():
-    drag_distribution
-
-
-def normal_stress_stringer(moment_lift, moment_drag, z_location, x_location):
-    sigma_stringer = (moment_lift*z_location)/Ixx + (moment_drag*x_location)/Izz
+#normal stress stringers due to bending
+def string_stress_normal(bl):
+    M_z = z_moment(y)
+    M_x = x_moment(y)
+    I = inertia(y)
+    I_zz = I[1]
+    I_xx = I[0]
+    I_xz = 0
+    x = #max distance to centroid
+    z = #max distance to centroid
+    sigma = ((((M_x*I_zz)-(M_z*I_xz))*z)+(((M_z*I_xx)-(M_x*I_xz))*x))/((I_xx*I_yy)-(I_xz)**2)
+    return sigma
