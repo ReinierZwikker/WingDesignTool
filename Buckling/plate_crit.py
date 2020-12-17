82c121f78cfcdeb8d97b2ccbd0be1d5ce335b5df
Gidzo16 <groeneveldgideon@gmail.com>
2020-12-17 18:00:39 +0100

    kc added

--- Buckling/plate_crit.py
+++ Buckling/plate_crit.py
@@ -14,6 +14,7 @@ database_connector = DatabaseConnector()
 
 pois = database_connector.load_wingbox_value("poisson_ratio")
 E = database_connector.load_wingbox_value("youngs_modulus_pa")
+kc = 7.3
 t = database_connector.load_wingbox_value("plate_thickness")
 points = database_connector.load_wingbox_value("wingbox_corner_points")
 top_lim1 = database_connector.load_wingbox_value("top_stringer_lim_point_1")
@@ -36,8 +37,6 @@ def stringer_spacing(y):
 
 
 def plate_crit_force(y): #Kc needs to be determined manually per section
-    if y < 10:
-        kc = 
     Fcr = (((pi**2)*kc*E) / (12*(1-(pois**2)))) * ((t/stringer_spacing(y))**2)
     return Fcr
 
@@ -45,3 +44,5 @@ def plate_crit_force(y): #Kc needs to be determined manually per section
 def plate_crit_stress(y):
     sigma = plate_crit_force(y) / (t*stringer_spacing(y))
     return sigma
+
+print(stringer_spacing(0))
